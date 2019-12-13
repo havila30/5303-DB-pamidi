a6576bd7b10890d8b6a0c56f522405f230264309
havila30 <46728785+havila30@users.noreply.github.com>
2019-12-13 12:09:08 -0600

    Add files via upload

---
+++ A09/markovify.py
@@ -0,0 +1,13 @@
+import markovify
+
+with open("grimms_fairy_tales.txt", errors ="ignore") as f:
+
+for i in range(5):
+    print(Text_sentence())
+
+print ("\n")
+
+for i in range(1000000):
+    print(model_combo.make_short_sentence(280))
+
+
